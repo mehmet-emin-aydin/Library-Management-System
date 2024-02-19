73decdc5e0c194a01b630d9be9a1f1661c75b608
Mehmet Emin Aydin <16mehmet.emin@gmail.com>
2024-02-19 23:00:11 +0300

    gitignore created

--- library_v2.py
+++ library_v2.py
@@ -1,41 +1,50 @@
 class Library:
     def __init__(self, file_name="books.txt"):
+        """Initialize the Library with a file name and an empty book list."""
         self.file_name = file_name
-        self.file = open(self.file_name, 'a+')
-        self.book_list = list()
+        self.file = open(self.file_name, 'a+')  # Open the file for reading and appending
+        self.book_list = list()  # Initialize an empty list to store books
 
     def __del__(self):
+        """Destructor to close the file when the object is deleted."""
         self.file.close()
 
     def read_books(self):
-        self.file.seek(0)
+        """Read the books from the file and populate the book list."""
+        self.file.seek(0)  # Move the file pointer to the beginning
+        # Read each line from the file, strip newline characters, and split by comma to get book details
         self.book_list = [list(book.rstrip("\n").split(',')) for book in self.file.readlines()]
 
     def list_books(self):
+        """List all the books stored in the library."""
         try:
             print("Name of Book".center(41, " ") + "|" + "Author of Book".center(39, " "))
-            self.read_books()
+            self.read_books()  # Read books from the file
             for i, book in enumerate(self.book_list):
                 print(f'{i + 1}.' + book[0].ljust(39) + "|" + book[1].ljust(39))
         except Exception as e:
             print("\nError occurred while listing the Books\n" + str(e))
 
     def add_book(self, book: list):
+        """Add a new book to the library."""
         try:
+            # Write the book details to the file separated by commas
             self.file.write("\n" + ",".join(book))
             print("\nThe Book Successfully Added to Library")
         except Exception as e:
             print("\nError occurred while adding the Book\n" + str(e))
 
     def del_book(self, book_title: str):
+        """Remove a book from the library."""
         try:
-            self.read_books()
+            self.read_books()  # Read books from the file
             found_books = [book for book in self.book_list if book[0].lower() == book_title.lower()]
             if found_books:
                 for book in found_books:
-                    self.book_list.remove(book)
-                self.file.seek(0)
-                self.file.truncate()
+                    self.book_list.remove(book)  # Remove the book from the list
+                self.file.seek(0)  # Move the file pointer to the beginning
+                self.file.truncate()  # Clear the file contents
+                # Write the updated book list to the file
                 self.file.writelines([",".join(book) + "\n" for book in self.book_list])
                 print(f"\n'{book_title}' Successfully Removed From Library")
             else:
@@ -44,8 +53,10 @@ class Library:
             print("\nError occurred while removing the Book\n" + str(e))
 
     def get_matching_books(self, search_title: str):
+        """Find books matching the given search title."""
         try:
-            self.read_books()
+            self.read_books()  # Read books from the file
+            # Return a list of books where the search title is found (case-insensitive)
             return [book for book in self.book_list if search_title.lower() in book[0].lower()]
         except Exception as e:
             print("\nError occurred while searching for matching books\n" + str(e))
--- library_v2_interface.py
+++ library_v2_interface.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout, QStackedLayout, QMessageBox
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit, QVBoxLayout, QStackedLayout, QMessageBox, QHBoxLayout, QFormLayout
 
 from library_v2 import Library
 
@@ -12,111 +12,104 @@ class LibraryGUI(QWidget):
 
     def init_ui(self):
         self.stacked_layout = QStackedLayout()
-
-        # Main Menu UI
-        self.main_menu_widget = QWidget()
-        main_layout = QVBoxLayout()
-        self.button_list_books = QPushButton("List Books")
-        self.button_add_book = QPushButton("Add Book")
-        self.button_remove_book = QPushButton("Remove Book")
-        self.button_exit = QPushButton("Exit")
-        self.button_list_books.clicked.connect(self.list_books)
-        self.button_add_book.clicked.connect(self.show_add_book_form)
-        self.button_remove_book.clicked.connect(self.show_remove_book_layout)
-        self.button_exit.clicked.connect(self.close)
-        main_layout.addWidget(self.button_list_books)
-        main_layout.addWidget(self.button_add_book)
-        main_layout.addWidget(self.button_remove_book)
-        main_layout.addWidget(self.button_exit)
-        self.main_menu_widget.setLayout(main_layout)
-
-        # List Books UI
-        self.list_books_widget = QWidget()
-        self.list_books_layout = QVBoxLayout()
-        self.label_books = QLabel("Books:")
-        self.list_books_layout.addWidget(self.label_books)
-        self.list_books_widget.setLayout(self.list_books_layout)
-
-        # Add Book UI
-        self.add_book_widget = QWidget()
-        add_book_layout = QVBoxLayout()
-        self.label_title = QLabel("Title:")
-        self.label_author = QLabel("Author:")
-        self.label_release_year = QLabel("Release Year:")
-        self.label_num_pages = QLabel("Number of Pages:")
-        self.lineedit_title = QLineEdit()
-        self.lineedit_author = QLineEdit()
-        self.lineedit_release_year = QLineEdit()
-        self.lineedit_num_pages = QLineEdit()
-        self.button_save = QPushButton("Save")
-        self.button_cancel = QPushButton("Cancel")
-        self.button_save.clicked.connect(self.save_book)
-        self.button_cancel.clicked.connect(self.restore_main_menu)
-        add_book_layout.addWidget(self.label_title)
-        add_book_layout.addWidget(self.lineedit_title)
-        add_book_layout.addWidget(self.label_author)
-        add_book_layout.addWidget(self.lineedit_author)
-        add_book_layout.addWidget(self.label_release_year)
-        add_book_layout.addWidget(self.lineedit_release_year)
-        add_book_layout.addWidget(self.label_num_pages)
-        add_book_layout.addWidget(self.lineedit_num_pages)
-        add_book_layout.addWidget(self.button_save)
-        add_book_layout.addWidget(self.button_cancel)
-        self.add_book_widget.setLayout(add_book_layout)
-
-        # Remove Book UI
-        self.remove_book_widget = QWidget()
-        remove_book_layout = QVBoxLayout()
-        self.label_remove_title = QLabel("Enter the Title of the Book to Remove:")
-        self.lineedit_remove_title = QLineEdit()
-        self.button_remove = QPushButton("Remove")
-        self.button_cancel1 = QPushButton("Cancel")
-        self.button_remove.clicked.connect(self.remove_book)
-        self.button_cancel1.clicked.connect(self.restore_main_menu)
-        remove_book_layout.addWidget(self.label_remove_title)
-        remove_book_layout.addWidget(self.lineedit_remove_title)
-        remove_book_layout.addWidget(self.button_remove)
-        remove_book_layout.addWidget(self.button_cancel1)
-        self.remove_book_widget.setLayout(remove_book_layout)
-
-        self.stacked_layout.addWidget(self.main_menu_widget)
-        self.stacked_layout.addWidget(self.list_books_widget)
-        self.stacked_layout.addWidget(self.add_book_widget)
-        self.stacked_layout.addWidget(self.remove_book_widget)
-
+        self.create_main_menu()
+        self.create_list_books_layout()
+        self.create_add_book_layout()
+        self.create_remove_book_layout()
         self.setLayout(self.stacked_layout)
 
-    def show_add_book_form(self):
-        self.stacked_layout.setCurrentIndex(2)  # Show add book form
+    def create_main_menu(self):
+        buttons = [
+            ("List Books", self.list_books),
+            ("Add Book", self.show_add_book_form),
+            ("Remove Book", self.show_remove_book_layout),
+            ("Exit", self.close)
+        ]
+        self.stacked_layout.addWidget(self.create_widget_with_buttons(buttons))
+
+    def create_list_books_layout(self):
+        self.list_books_widget = self.create_widget_with_layout(QVBoxLayout(), [QLabel("Books:")])
+        self.stacked_layout.addWidget(self.list_books_widget)
+
+    def create_add_book_layout(self):
+        labels = ["Title:", "Author:", "Release Year:", "Number of Pages:"]
+        line_edits = [QLineEdit() for _ in range(len(labels))]
+        self.line_edits = line_edits
+        form_layout = QFormLayout()
+        for label, line_edit in zip(labels, line_edits):
+            form_layout.addRow(label, line_edit)
+        buttons = [("Save", self.save_book), ("Cancel", self.restore_main_menu)]
+        button_layout = QHBoxLayout()
+        for text, callback in buttons:
+            button = QPushButton(text)
+            button.clicked.connect(callback)
+            button_layout.addWidget(button)
+        add_book_layout = QVBoxLayout()
+        add_book_layout.addLayout(form_layout)
+        add_book_layout.addLayout(button_layout)
+        add_book_widget = QWidget()
+        add_book_widget.setLayout(add_book_layout)
+        self.stacked_layout.addWidget(add_book_widget)
+
+    def create_remove_book_layout(self):
+        remove_layout = QVBoxLayout()
+        self.lineedit_remove_title = QLineEdit()
+        buttons = [("Remove", self.remove_book), ("Cancel", self.restore_main_menu)]
+        remove_layout.addWidget(QLabel("Enter the Title of the Book to Remove:"))
+        remove_layout.addWidget(self.lineedit_remove_title)
+        remove_layout.addStretch(1)
+        button_layout = QHBoxLayout()
+        for text, callback in buttons:
+            button = QPushButton(text)
+            button.clicked.connect(callback)
+            button_layout.addWidget(button)
+        remove_layout.addLayout(button_layout)
+        self.remove_book_widget = QWidget()
+        self.remove_book_widget.setLayout(remove_layout)
+        self.stacked_layout.addWidget(self.remove_book_widget)
+
+    def create_widget_with_buttons(self, button_info):
+        layout = QVBoxLayout()
+        for text, callback in button_info:
+            button = QPushButton(text)
+            button.clicked.connect(callback)
+            layout.addWidget(button)
+        widget = QWidget()
+        widget.setLayout(layout)
+        return widget
+
+    def create_widget_with_layout(self, layout, widgets=None):
+        if widgets:
+            for widget in widgets:
+                layout.addWidget(widget)
+        widget = QWidget()
+        widget.setLayout(layout)
+        return widget
 
     def save_book(self):
-        title = self.lineedit_title.text()
-        author = self.lineedit_author.text()
-        release_year = self.lineedit_release_year.text()
-        num_pages = self.lineedit_num_pages.text()
-        book_info = [title, author, release_year, num_pages]
+        title = self.line_edits[0].text()
+        if not title.strip():
+            QMessageBox.warning(self, 'Warning', "Please enter the title of the book!")
+            return
+        book_info = [lineedit.text() for lineedit in self.line_edits]
         self.library.add_book(book_info)
         self.restore_main_menu()
 
     def list_books(self):
-        # Clear the previous list of books
-        for i in reversed(range(self.list_books_layout.count())):
-            widget = self.list_books_layout.itemAt(i).widget()
-            if widget is not None:
-                widget.setParent(None)
+        self.clear_layout(self.list_books_widget.layout())
         self.library.read_books()
         for book in self.library.book_list:
-            self.list_books_layout.addWidget(QLabel(f"{book[0]} by {book[1]}"))
-        self.button_close = QPushButton("Close")
-        self.button_close.clicked.connect(self.restore_main_menu)
-        self.list_books_layout.addWidget(self.button_close)
-        self.stacked_layout.setCurrentIndex(1)  # Show list of books
-
-    def show_remove_book_form(self):
-        self.stacked_layout.setCurrentIndex(3)  # Show remove book form
+            self.list_books_widget.layout().addWidget(QLabel(f"{book[0]} by {book[1]}"))
+        button_close = QPushButton("Close")
+        button_close.clicked.connect(self.restore_main_menu)
+        self.list_books_widget.layout().addWidget(button_close)
+        self.stacked_layout.setCurrentIndex(1)
 
     def remove_book(self):
         title_to_remove = self.lineedit_remove_title.text()
+        if not title_to_remove.strip():
+            QMessageBox.warning(self, 'Warning', "Please enter the title of the book to remove!")
+            return
         matching_books = self.library.get_matching_books(title_to_remove)
         if matching_books:
             for book in matching_books:
@@ -129,10 +122,19 @@ class LibraryGUI(QWidget):
             QMessageBox.warning(self, 'Warning', f"No books found with the title '{title_to_remove}'!")
 
     def restore_main_menu(self):
-        self.stacked_layout.setCurrentIndex(0)  # Show main menu
+        self.stacked_layout.setCurrentIndex(0)
+
+    def show_add_book_form(self):
+        self.stacked_layout.setCurrentIndex(2)
 
     def show_remove_book_layout(self):
-        self.stacked_layout.setCurrentIndex(3)  # Show remove book form
+        self.stacked_layout.setCurrentIndex(3)
+
+    def clear_layout(self, layout):
+        for i in reversed(range(layout.count())):
+            widget = layout.itemAt(i).widget()
+            if widget is not None:
+                widget.setParent(None)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
